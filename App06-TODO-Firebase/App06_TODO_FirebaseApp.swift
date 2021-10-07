df267feaba4ca5d602bd045903ad4fecf3a56917
David Josué Marcial Quero <A00828702@itesm.mx>
2021-10-07 09:21:35 -0700

    Initial Commit

---
+++ App06-TODO-Firebase/App06_TODO_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  App06_TODO_FirebaseApp.swift
+//  App06-TODO-Firebase
+//
+//  Created by David Josué Marcial Quero on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct App06_TODO_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
